92971573a1b072011fe0c034b08f8c9f3428c2bd
havefive <lzc09008@gmail.com>
2017-03-23 17:50:08 +0800

    add element-ui and axios example

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,34 +1,38 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <el-button>element-ui默认按钮</el-button>
+    <el-button type="primary" v-on:click="getData">axios get请求</el-button>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'hello',
     data () {
       return {
-        msg: 'Welcome to Your Vue.js App'
+        msg: 'Welcome to Your Vue Template Project'
+      }
+    },
+    methods: {
+      getData: function () {
+        let that = this
+        axios.get('/user?ID=12345')
+        .then(function (response) {
+          console.log(response)
+        })
+        .catch(function (error) {
+          console.log(error)
+          that.$message({
+            message: 'error:404',
+            type: 'warning'
+          })
+        })
       }
     }
   }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
